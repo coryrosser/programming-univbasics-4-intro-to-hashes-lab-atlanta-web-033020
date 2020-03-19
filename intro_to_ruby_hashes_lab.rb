1edf35390624d0b6ae01180638f02bd5d9959ba9
Cortlyn Rosser <coryrosser@gmail.com>
2020-03-19 15:02:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,11 +22,11 @@ def read_from_hash(hash, key)
   return hash[key]
 end
 
-def update_counting_hash(hash, key)if hash[key]
+def update_counting_hash(hash, key)
+  if hash[key]
   hash[key] += 1
 else
   hash[key] = 1
 end
-  hash[key] ? hash[key] += 1 : hash[key] = 1 
 end
 
